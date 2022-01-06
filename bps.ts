d31510a6f805e4105e639bb30411f55e471e054a
liamhtml <liampreisser5@gmail.com>
2022-01-05 21:52:09 -0500

    added calcbps function

---
+++ bps.ts
@@ -0,0 +1,20 @@
+import { banaKv } from './main';
+import { buildings } from './consts';
+import { upgradeRate } from './consts';
+
+export async function calcBps(id: string) {
+    let bps = 0;
+    // @ts-ignore
+    let userObj = await banaKv.get<jsonObj>(id);
+    let userBuildings = Object.keys(userObj.buildings);
+    for (let i = 0; i < userBuildings.length; i++) {
+        let building = userObj.buildings[userBuildings[i]];
+        // @ts-ignore
+        let baseBuildingBps = buildings[userBuildings[i]].baseBpS;
+        let numOfBuilding = building.num;
+        let totalBuildingBps =
+            numOfBuilding * (baseBuildingBps + upgradeRate * building.lvl);
+        bps = bps + totalBuildingBps;
+    }
+    return bps;
+}
